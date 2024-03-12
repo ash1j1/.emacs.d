4824cfc5b83ca35d8d223b0e8754ce3593391ff6
Ashlyn Jacob <ashlyn.jacob20@gmail.com>
2024-03-12 21:34:47 +0530

    no more EC2 tests

--- init.el
+++ init.el
@@ -7,8 +7,8 @@
 
 (menu-bar-mode -1)            ; Disable the menu bar
 
-;; Set up the visible bell
-(setq visible-bell t)
+;; Disable the visible bell
+(setq visible-bell nil)
 
 
 (load-theme 'wombat)
@@ -104,16 +104,3 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
