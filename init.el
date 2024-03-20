3d3a3e337711ff47ba6581ad61366bb65455e33a
Ashlyn Jacob <ashlyn.jacob20@gmail.com>
2024-03-20 21:07:14 +0530

    remove the weird stuff

--- init.el
+++ init.el
@@ -109,16 +109,3 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons which-key rainbow-delimiters ivy-rich helpful doom-themes doom-modeline counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
